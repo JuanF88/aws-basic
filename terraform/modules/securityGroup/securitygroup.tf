b774e11541592227e58cffac23c0a511814b7694
joseimanul <josem123321@gmail.com>
2024-12-10 14:22:12 -0500

    update modules

--- terraform/modules/securityGroup/securitygroup.tf
+++ terraform/modules/securityGroup/securitygroup.tf
@@ -19,3 +19,12 @@ resource "aws_security_group_rule" "allow_http" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ec2_sg.id
 }
+
+resource "aws_security_group_rule" "allow_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
