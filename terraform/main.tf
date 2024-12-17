c55e905a471b340e6896e2e8e217979ec9dc8c5f
joseimanul <josem123321@gmail.com>
2024-12-17 10:35:07 -0500

    Updating Files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,36 @@
-# module "vpc" {
-#   source = "./modules/vpc"
+module "vpc" {
+  source               = "./modules/vpc"
+  environment          = "dev"
+  public_subnet_count  = 2
+  private_subnet_count = 2
+  cluster_name         = "ex-terraform"
+}
+
+module "security_group" {
+  source = "./modules/securityGroup"
+
+  instance_name               = "Security_group"
+  vpc_id                      = module.vpc.vpc_id
+  depends_on                  = [module.vpc]
+  private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+}
+
+# module "eks_module" {
+#   source              = "./modules/eks_module"
+#   public_subnet_ids = module.vpc.public_subnet_ids
 #   environment = "dev"
-#   public_subnet_count = 2
-#   private_subnet_count = 2
 #   cluster_name = "ex-terraform"
 # }
+
+module "mks_module" {
+  cluster_name                = "ex-terraform"
+  number_of_broker_nodes      = 2
+  source                      = "./modules/mks_module"
+  instance_type               = "kafka.t3.small"
+  private_subnets_cidr_blocks = module.vpc.private_subnet_ids
+  security_group_id           = module.security_group.security_group_id
+  depends_on                  = [module.security_group]
+}
 
 # module "eks" {
 #   source = "./modules/eks"
@@ -16,17 +42,12 @@
 #   #ec2_ssh_key_name   = "myLinux"
 # }
 
-module "mks" {
-  source             = "./modules/mks"
-}
-
-
-# module "security_group" {
-#   source = "./modules/securityGroup"
-
-#   instance_name = "asda"
-#   vpc_id        = module.vpc.vpc_id
-#   depends_on    = [module.vpc]
+# module "mks" {
+#   source = "./modules/mks"
+#   private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+#   private_subnets = module.vpc.private_subnet_ids
+#   vpc_id = module.vpc.vpc_id
+#   security_group_id = module.security_group.security_group_id
 # }
 
 
